568dee8305455c4b538f4bf532b2266e0d944fea
harcuracy <akandesoji4christ@gmail.com>
2025-11-12 08:27:07 +0100

    update

--- llm/job_selector.py
+++ llm/job_selector.py
@@ -4,7 +4,7 @@ import json
 import re
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_groq import ChatGroq
+
 
 from db.db import job_already_sent
 from .prompt import job_selection_prompt
